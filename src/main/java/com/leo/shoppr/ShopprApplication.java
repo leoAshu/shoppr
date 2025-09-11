21af6a83d44d6478ca0329645c5a0190bdddb912
leoAshu <ashutosh.ojha2009@gmail.com>
2025-09-11 19:51:53 -0400

    initial commit

---
+++ src/main/java/com/leo/shoppr/ShopprApplication.java
@@ -0,0 +1,13 @@
+package com.leo.shoppr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopprApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopprApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/leo/shoppr/ShopprApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leo.shoppr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopprApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
