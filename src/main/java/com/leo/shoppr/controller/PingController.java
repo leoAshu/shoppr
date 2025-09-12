063bd7c63a7c613f91ccbac28f120effaaa8ed9e
leoAshu <ashutosh.ojha2009@gmail.com>
2025-09-12 14:26:26 -0400

    replaces application.properties with profile-specific application.yml

--- src/main/java/com/leo/shoppr/controller/PingController.java
+++ src/main/java/com/leo/shoppr/controller/PingController.java
@@ -2,6 +2,7 @@ package com.leo.shoppr.controller;
 
 import com.leo.shoppr.response.CustomResponse;
 import com.leo.shoppr.response.ResponseStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class PingController {
 
+    @Value("${ping.message}")
+    private String message;
+
     @GetMapping("/ping")
     public ResponseEntity<CustomResponse<Void>> ping() {
         CustomResponse<Void> response = new CustomResponse<>();
 
         response.setStatus(ResponseStatus.SUCCESS);
-        response.setMessage("Shoppr is Live!");
+        response.setMessage(message);
 
         return ResponseEntity.ok(response);
     }
